d513c0ee3df4b896964da7ecccb32594632aef22
Oscar Nilsson <Oscar.Nilsson@biltema.com>
2023-12-04 13:31:19 +0100

    Part 1 day 4 2023

---
+++ 2023/4/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _4;
+
+class Program
+{
+    private const string filePath = "./input.txt";
+
+    static void Main(string[] args)
+    {
+        Part1();
+    }
+    
+    static void Part1()
+    {
+        using var reader = new StreamReader(filePath);
+
+        int total = 0;
+
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+
+            var winningNumbers = line.Split(":")[1].Split("|")[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            var scratchedNumbers = line.Split(":")[1].Split("|")[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            int points = 0;
+
+            foreach (var number in winningNumbers)
+            {
+                if (scratchedNumbers.Any(x => x == number))
+                {
+                    switch (points)
+                    {
+                        case 0:
+                            points++;
+                            break;
+                        default:
+                            points *= 2;
+                            break;
+                    }
+                }
+            }
+            total += points;
+        }
+
+        Console.WriteLine(total);
+    }
+}
