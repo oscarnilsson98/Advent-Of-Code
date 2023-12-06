bc82f07e57828960020e876f33c885abdb3f4b02
Oscar <69985026+oscarnilsson98@users.noreply.github.com>
2023-12-06 13:05:01 +0100

    Part 2 day 6 2023

--- 2023/6/Program.cs
+++ 2023/6/Program.cs
@@ -4,11 +4,10 @@ namespace _6;
 
 class Program
 {
-    private const string filePath = "input.txt";
-
     static void Main(string[] args)
     {
         Part1();
+        Part2();
     }
 
     static void Part1()
@@ -31,6 +30,39 @@ class Program
             for (int i = 0; i < Time; i++)
             {
                 int boatTraveled = i * (Time - i);
+                if (boatTraveled > Distance)
+                {
+                    iterationWins++;
+                }
+            }
+            wins.Add((Id, iterationWins));
+        }
+
+        foreach (var win in wins)
+        {
+            result *= win.wins;
+        }
+
+        Console.WriteLine(result);
+    }
+    
+    static void Part2()
+    {
+        int result = 1;
+
+        var constraints = new List<(int Id, double Time, double Distance)>
+        {
+            (1, 49787980, 298118510661181),
+        };
+
+        var wins = new List<(int Id, int wins)>();
+
+        foreach (var (Id, Time, Distance) in constraints)
+        {
+            int iterationWins = 0;
+            for (int i = 0; i < Time; i++)
+            {
+                double boatTraveled = i * (Time - i);
                 if (boatTraveled > Distance)
                 {
                     iterationWins++;
