f8b2aa46ee7405b4e2d6456bc42954cc7d2018a2
Oscar <69985026+oscarnilsson98@users.noreply.github.com>
2023-12-07 17:34:47 +0100

    Part 2 day 7 2023

--- 2023/7/Program.cs
+++ 2023/7/Program.cs
@@ -17,7 +17,8 @@ class Program
 
     static void Main(string[] args)
     {
-        Part1();
+        // Part1();
+        Part2();
     }
 
     static void Part1()
@@ -30,7 +31,7 @@ class Program
         {
             var hand = item.Split(" ")[0];
             var bid = int.Parse(item.Split(" ")[1]);
-            var type = GetHandTyp(hand);
+            var type = GetHandType(hand);
 
             handDictionary.Add(hand, (bid, (int)type));
         }
@@ -44,7 +45,40 @@ class Program
         Console.WriteLine(result);
     }
 
-    static HandTyp GetHandTyp(string hand)
+    static HandTyp GetHandType(string hand)
+    {
+        List<char> characters = [.. hand];
+
+        var charDictionary = new Dictionary<char, int>();
+
+        return GetHandTypeFromDictionary(charDictionary);
+    }
+
+    static void Part2()
+    {
+        var input = File.ReadAllLines(filePath);
+
+        var handDictionary = new Dictionary<string, (int bid, int type)>();
+
+        foreach (var item in input)
+        {
+            var hand = item.Split(" ")[0];
+            var bid = int.Parse(item.Split(" ")[1]);
+            var type = GetHandTypePart2(hand);
+
+            handDictionary.Add(hand, (bid, type));
+        }
+
+        var result = handDictionary
+            .OrderByDescending(x => x.Value.type)
+            .ThenBy(x => x.Key, new CustomComparer())
+            .Select((x, index) => x.Value.bid * (index + 1))
+            .Sum();
+
+        Console.WriteLine(result);
+    }
+
+    static int GetHandTypePart2(string hand)
     {
         List<char> characters = [.. hand];
 
@@ -55,6 +89,30 @@ class Program
             charDictionary.Add(p, characters.Where(x => x == p).Count());
         }
 
+        var possibleTypes = new List<int>
+        {
+            (int)GetHandTypeFromDictionary(charDictionary)
+        };
+
+        if (charDictionary.TryGetValue('J', out int value))
+        {
+            foreach (var current in charDictionary.Where(x => x.Key != 'J'))
+            {
+                var replacedCharDictionary = charDictionary.Where(x => x.Key != 'J').ToDictionary(entry => entry.Key, entry => entry.Value);
+
+                replacedCharDictionary[current.Key] = current.Value + value;
+
+                possibleTypes.Add((int)GetHandTypeFromDictionary(replacedCharDictionary));
+            }
+        }
+
+        var firsto = possibleTypes.OrderBy(x => x).First();
+
+        return firsto;
+    }
+
+    static HandTyp GetHandTypeFromDictionary(Dictionary<char, int> charDictionary)
+    {
         if (charDictionary.Count == 1)
         {
             return HandTyp.fiveOfAKind;
@@ -89,7 +147,7 @@ class Program
 
 public class CustomComparer : IComparer<string>
 {
-    public readonly string customOrder = "AKQJT98765432";
+    public readonly string customOrder = "AKQT98765432J";
 
     public int Compare(string? x, string? y)
     {
