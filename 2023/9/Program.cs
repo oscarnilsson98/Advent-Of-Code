f79b479706a4c862203589c7964f952288f14b1e
Oscar <69985026+oscarnilsson98@users.noreply.github.com>
2023-12-09 12:23:54 +0100

    Part 2 day 9 2023

--- 2023/9/Program.cs
+++ 2023/9/Program.cs
@@ -6,6 +6,7 @@ class Program
     static void Main()
     {
         Part1();
+        Part2();
     }
 
     static void Part1()
@@ -37,6 +38,38 @@ class Program
         {
             1 => numberArray.Last() + newNumberList.Last(),
             _ => numberArray.Last() + FindNextValue([.. newNumberList]),
+        };
+    }
+
+    static void Part2()
+    {
+        var input = File.ReadAllLines(filePath);
+
+        long total = 0;
+        foreach (var line in input)
+        {
+            total += FindNextValueBackwards(line.Split(" ").Select(x => long.Parse(x)).ToArray());
+        }
+
+        Console.WriteLine(total);
+    }
+
+    static long FindNextValueBackwards(long[] numberArray)
+    {
+        var newNumberList = new List<long>();
+        for (int i = 0; i < numberArray.Length; i++)
+        {
+            if ((i + 1) < numberArray.Length)
+            {
+                long difference = numberArray[i + 1] - numberArray[i];
+                newNumberList.Add(difference);
+            }
+        }
+
+        return newNumberList.All(x => x == 0) switch
+        {
+            true => numberArray.First() - newNumberList.First(),
+            _ => numberArray.First() - FindNextValueBackwards([.. newNumberList]),
         };
     }
 }
