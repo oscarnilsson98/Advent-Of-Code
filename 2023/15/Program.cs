fe2172a0ce5fa8b43206c6796043aabe413c23ac
Oscar <69985026+oscarnilsson98@users.noreply.github.com>
2023-12-15 13:10:17 +0100

    Part 1 Day 15 2023

---
+++ 2023/15/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _15;
+
+class Program
+{
+    // private const string filePath = "./easyExampleInput.txt";
+    // private const string filePath = "./exampleInput.txt";
+    private const string filePath = "./input.txt";
+
+    static void Main()
+    {
+        Part1();
+    }
+
+    static void Part1()
+    {
+        var input = File.ReadAllLines(filePath)[0].Split(",");
+
+        var totalSum = 0;
+        foreach (var step in input)
+        {
+            var currentValue = 0;
+            foreach (var character in step)
+            {
+                // Determine the ASCII code for the current character of the string.
+                var asciiCode = (int)character;
+
+                // Increase the current value by the ASCII code you just determined.
+                currentValue += asciiCode;
+
+                // Set the current value to itself multiplied by 17.
+                currentValue *= 17;
+
+                // Set the current value to the remainder of dividing itself by 256.
+                currentValue %= 256;
+            }
+            totalSum += currentValue;
+        }
+
+        Console.WriteLine($"Total Sum: {totalSum}");
+    }
+}
