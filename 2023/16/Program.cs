8a596e2af3417f2863505f040f919f7c3aa99333
Oscar <69985026+oscarnilsson98@users.noreply.github.com>
2023-12-17 00:05:31 +0100

    Part 2 day 16 2023

--- 2023/16/Program.cs
+++ 2023/16/Program.cs
@@ -2,15 +2,16 @@
 
 class Program
 {
-    private const string filePath = "./input.txt";
     // private const string filePath = "./exampleInput.txt";
+    private const string filePath = "./input.txt";
 
     private static int rowBorder = -1;
     private static int columnBorder = -1;
 
     static void Main()
     {
-        Part1();
+        // Part1();
+        Part2();
     }
 
     static void Part1()
@@ -27,6 +28,55 @@ class Program
         passedSpots = MoveDown(input, 0, 0, passedSpots);
 
         Console.WriteLine(passedSpots.Select(x => new { x.row, x.column }).Distinct().Count());
+    }
+
+    static void Part2()
+    {
+        var input = File.ReadAllLines(filePath);
+
+        rowBorder = input.Length - 1;
+        columnBorder = input[0].Length - 1;
+
+        var allTriesScores = new List<int>();
+
+        // from above
+        for (int i = 0; i < input[0].Length; i++)
+        {
+            HashSet<(int row, int column, char direction)> passedSpots = [];
+
+            passedSpots = MoveDown(input, -1, i, passedSpots);
+
+            allTriesScores.Add(passedSpots.Select(x => new { x.row, x.column }).Distinct().Count());
+        }
+        // from below
+        for (int i = 0; i < input[0].Length; i++)
+        {
+            HashSet<(int row, int column, char direction)> passedSpots = [];
+
+            passedSpots = MoveUp(input, rowBorder + 1, i, passedSpots);
+
+            allTriesScores.Add(passedSpots.Select(x => new { x.row, x.column }).Distinct().Count());
+        }
+        // from left
+        for (int i = 0; i < input.Length; i++)
+        {
+            HashSet<(int row, int column, char direction)> passedSpots = [];
+
+            passedSpots = MoveRight(input, i, -1, passedSpots);
+
+            allTriesScores.Add(passedSpots.Select(x => new { x.row, x.column }).Distinct().Count());
+        }
+        // from right
+        for (int i = 0; i < input.Length; i++)
+        {
+            HashSet<(int row, int column, char direction)> passedSpots = [];
+
+            passedSpots = MoveLeft(input, i, columnBorder + 1, passedSpots);
+
+            allTriesScores.Add(passedSpots.Select(x => new { x.row, x.column }).Distinct().Count());
+        }
+
+        Console.WriteLine(allTriesScores.Max());
     }
 
     static HashSet<(int row, int column, char direction)> MoveUp(string[] input, int previousRow, int previousColumn, HashSet<(int row, int column, char direction)> passedSpots)
